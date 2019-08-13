5e1d000fabf9a44a9abbff3ba104f9a33d03d9d9
chents <1011186201@qq.com>
2019-08-13 15:59:50 +0800

    Initial Commit

---
+++ tomcatGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tomcatGame
+//
+//  Created by 陈统帅 on 2019/8/13.
+//  Copyright © 2019 陈统帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
